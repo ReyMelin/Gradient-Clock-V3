d23d25ac71c584c8cdddbb4d8ebb6243a0c622ae
Rachel Hermann <reymelin2024@gmail.com>
2026-02-03 22:17:04 -0700

    fix: register snapshot plugin

--- android/app/src/main/java/com/reymelin/gradientclock/MainActivity.java
+++ android/app/src/main/java/com/reymelin/gradientclock/MainActivity.java
@@ -7,7 +7,6 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Register the Snapshot plugin (auto-registered via @CapacitorPlugin annotation)
         registerPlugin(ClockSnapshotPlugin.class);
     }
 }
